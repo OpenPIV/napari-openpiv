6fda0a8b53826ad406889de7a9e7c68ab8c92842
Alex Liberzon <alexlib@users.noreply.github.com>
2022-04-04 22:38:23 +0300

    some prototype but not working in the vector plot

--- src/napari_openpiv/_widget.py
+++ src/napari_openpiv/_widget.py
@@ -30,8 +30,30 @@ class ExampleQWidget(QWidget):
 
 
 @magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
+def example_magic_widget(img_layer: "napari.layers.Image", 
+                         viewer: "napari.viewer.Viewer"):
     print(f"you have selected {img_layer}")
+    from openpiv.piv import simple_piv
+    import numpy as np
+    # assuming the layer is a stack
+    I = np.array(img_layer.data[0]).astype(np.int32)
+    J = np.array(img_layer.data[1]).astype(np.int32)
+    x,y,u,v = simple_piv(I,J, plot=False)
+    # sample vector image-like data
+    n = np.prod(x.shape)
+    # n x m grid of slanted lines
+    # random data on the open interval (-1, 1)
+    pos = np.zeros(shape=(n, 2, 2), dtype=np.float32)
+
+    # assign projections for each vector
+    pos[:, 0, 0] = x.flatten()
+    pos[:, 0, 1] = y.flatten()
+    pos[:, 1, 0] = x.flatten() + u.flatten()
+    pos[:, 1, 1] = y.flatten() + v.flatten()
+
+    # add the vectors
+    vect = viewer.add_vectors(pos, edge_width=0.2, length=2.5)
+
 
 
 # Uses the `autogenerate: true` flag in the plugin manifest
